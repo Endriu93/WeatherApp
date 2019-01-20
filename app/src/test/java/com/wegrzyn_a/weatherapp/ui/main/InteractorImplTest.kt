e2cb605273fed338193829da835c7859f1e6bff7
Andrzej Węgrzyn <andrzej9393@gmail.com>
2019-01-20 16:06:45 +0100

    create helper functions for mocks

--- app/src/test/java/com/wegrzyn_a/weatherapp/ui/main/InteractorImplTest.kt
+++ app/src/test/java/com/wegrzyn_a/weatherapp/ui/main/InteractorImplTest.kt
@@ -4,15 +4,14 @@ import com.wegrzyn_a.weatherapp.data.DataSource
 import com.wegrzyn_a.weatherapp.data.model.Measurement
 import com.wegrzyn_a.weatherapp.data.model.Station
 import com.wegrzyn_a.weatherapp.data.model.Weather
+import com.wegrzyn_a.weatherapp.mockLocation
+import com.wegrzyn_a.weatherapp.mockDataSourceMeasurement
+import com.wegrzyn_a.weatherapp.mockDataSourceStations
 import com.wegrzyn_a.weatherapp.sensor.LocationProvider
-import io.reactivex.Scheduler
-import io.reactivex.Single
-import io.reactivex.schedulers.Schedulers
 import io.reactivex.schedulers.TestScheduler
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
-import org.mockito.Mockito.`when`
 import org.mockito.Mockito.verify
 import org.mockito.MockitoAnnotations
 
@@ -49,32 +48,14 @@ class InteractorImplTest {
         val long = 19.93658
         val woeid = 523920
         val temp = "3"
-        `when`(locationProvider.getLattLong()).thenReturn(Pair(latt, long))
-        `when`(dataSource.getStations(latt, long)).thenReturn(
-            Single.just(
-                listOf(
-                    Station(
-                        "...",
-                        woeid
-                    )
-                )
-            )
-        )
-        `when`(dataSource.getMeasurements(woeid)).thenReturn(
-            Single.just(
-                Measurement(
-                    listOf(
-                        Weather(
-                            temp
-                        )
-                    )
-                )
-            )
-        )
+        mockLocation(locationProvider,long, latt)
+        mockDataSourceStations(dataSource,latt, long, listOf(Station("...",woeid)))
+        mockDataSourceMeasurement(dataSource,woeid, Measurement(listOf(Weather(temp))))
 
         interactor.getTemps(onSuccess)
         scheduler.triggerActions()
 
         verify(onSuccess).invoke(listOf(temp))
     }
+
 }
